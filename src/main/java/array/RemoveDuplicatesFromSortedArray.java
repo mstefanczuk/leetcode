2067a5027ce78b5e92567de01cbee6af0b3c1370
Michal Stefanczuk <mstefanczuk@bluesoft.net.pl>
2017-11-21 13:02:16 +0100

    Update RemoveDuplicatesFromSortedArray solution

--- src/main/java/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/array/RemoveDuplicatesFromSortedArray.java
@@ -6,50 +6,23 @@ package array;
 * Source: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 *
 * Solution description:
-* We iterate through the whole loop starting with second element.
-* If array length is 0 or 1, then we just do not get into the loop and return given length.
-* If length is greater than 1, we start with second element and compare it with previous one.
-* And while there is duplicate of previous item, we just skip this element and decrease length to be returned.
-* For this is j variable, which is help variable to skip these duplicates.
-* And if nums[j] is not duplicate of our previous (nums[i-1]) element, we place the nums[j] element into the
-* nums[i] place.
+* Our "new array" will be first newLength-1 elements of input array, so we just have to modify the beginning of the
+* input array.
+* We create variable j, which will be index of the "new array" - an array with duplicates removed.
+* We start with second element and iterate trough whole array. If the current i-element is a duplicate of the
+* previous one, we just do nothing and continue. And if they are not duplicates, we place current element on the
+* j-index place and increase j by one. This increased j is going to be the new length of the "new array". And that's it.
 * */
 
 class RemoveDuplicatesFromSortedArray {
 
     int removeDuplicates(int[] nums) {
-
-        int newLength = nums.length;
-        int j = 0;
-
-        for(int i = 1; i < nums.length; i++) {
-            j++;
-            while(j < nums.length && nums[i-1] == nums[j]) {
-                newLength--;
-                j++;
-            }
-
-            if(j < nums.length)
-                nums[i] = nums[j];
-        }
-
-        return newLength;
-    }
-
-    /*
-     * In below solution we replace elements until the end of array is reached.
-     */
-    int removeDuplicatesSlowVersion(int[] nums) {
-        int newLength = nums.length;
         int j = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i-1] != nums[i]) {
-                nums[j] = nums[i];
-                j++;
-            } else {
-                newLength--;
+                nums[j++] = nums[i];
             }
         }
-        return newLength;
+        return j;
     }
 }
